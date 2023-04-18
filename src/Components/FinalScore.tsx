51b3df32b3bfcfe466099481fd1c08f5ec7d33e0
= <=>
2023-04-18 14:10:32 -0300

    recomendations cards

--- src/Components/FinalScore.tsx
+++ src/Components/FinalScore.tsx
@@ -1,3 +1,5 @@
+import { ClockIcon, PlayIcon } from "../icons/icons";
+
 interface Props {
   score: number;
   handleReset: () => void;
@@ -18,6 +20,48 @@ const FinalScore = ({ score, handleReset }: Props) => (
         : score >= 20
         ? "Your score suggests but does not diagnose alcohol dependence syndrome, which may require a referral to specialized treatment."
         : ""}
+    </div>
+    <div className="flex flex-col space-y-6 mb-6 mt-6 justify-center items-center">
+      <div className="w-full h-32 bg-white shadow-xl rounded-lg grid grid-cols-3">
+        <div
+            className="grow border rounded-tr-full rounded-br-full rounded-bl-lg rounded-tl-lg bg-cover bg-center bg-no-repeat p-4"
+            style={{
+            backgroundImage: `url(/meditation.jpg)`,
+            }}
+        />
+        <div className="text-left flex flex-row justify-between p-4 col-span-2 "> 
+            <div className="flex flex-col space-y-3 md:ml-4 mt-auto mb-auto">
+                <p className="font-bold">Mindfulness techniques</p>
+                <div className="flex flex-row text-xs text-gray-500">
+                    <ClockIcon/>
+                    <p>10 min</p>
+                </div>
+            </div>
+            <button onClick={() => window.open("https://www.youtube.com/watch?v=O-6f5wQXSu8&ab_channel=Goodful", "_blank", "noopener noreferrer")} className="mt-auto _blank mb-auto bg-[#FFECC2] p-1 rounded-lg">
+                <PlayIcon />    
+            </button> 
+        </div>
+      </div>
+      <div className="w-full h-32 bg-white shadow-xl rounded-lg grid grid-cols-3">
+      <div
+            className="grow border rounded-tr-full rounded-br-full rounded-bl-lg rounded-tl-lg bg-cover bg-center bg-no-repeat p-4"
+            style={{
+            backgroundImage: `url(/suport.jpeg)`,
+            }}
+        />
+        <div className="text-left flex flex-row justify-between p-4 col-span-2 "> 
+            <div className="flex flex-col space-y-3 md:ml-4 mt-auto mb-auto">
+                <p className="font-bold">The importance of support</p>
+                <div className="flex flex-row text-xs text-gray-500">
+                    <ClockIcon/>
+                    <p>2 min</p>
+                </div>
+            </div>
+            <button onClick={() => window.open("https://www.youtube.com/watch?v=O16Cz0dXF_Q&ab_channel=DouglasResearchCentre", "_blank", "noopener noreferrer")} className="mt-auto _blank mb-auto bg-[#FFECC2] p-1 rounded-lg">
+                <PlayIcon />    
+            </button> 
+        </div>
+      </div>
     </div>
     <button
       className="m-2 h-10 w-24 rounded-md bg-slate-200 hover:bg-slate-100 hover:shadow-xl transition-all duration-500"
